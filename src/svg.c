0ebcb5c09a5d6176d707ca40f26cf916c2545b18
Nathan Caroba <nathancaroba01@gmail.com>
2025-10-06 23:59:15 -0300

    feat(svg): adiciona .h e .c(sem implementação)

---
+++ src/svg.c
@@ -0,0 +1,7 @@
+//
+// Created by natha on 04/10/2025.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "svg.h"
---
+++ src/svg.h
@@ -0,0 +1,8 @@
+//
+// Created by natha on 04/10/2025.
+//
+
+#ifndef CLION_SVG_H
+#define CLION_SVG_H
+
+#endif //CLION_SVG_H
