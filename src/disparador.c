94b0a855892ed1551ee1d21a32e0844df969d79c
Nathan Caroba <nathancaroba01@gmail.com>
2025-10-15 20:59:13 -0300

    feat(disparador): implementação do .c

--- src/disparador.c
+++ src/disparador.c
@@ -4,4 +4,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "carregador.h"
+#include "geo.h"
 #include "disparador.h"
+
+typedef struct {
+    int id;
+    double x;
+    double y;
+    Forma formaDisparo;
+    Carregador carregadorEsq;
+    Carregador carregadorDir;
+}disparador;
+
+Disparador CriarDisparador(int id,double x ,double y) {
+    disparador* d = (disparador*) malloc (sizeof (disparador));
+    if (d == NULL) {
+        printf("Erro ao alocar memoria!\n");
+        exit(1);
+    }
+    d->id = id;
+    d->x = x;
+    d->y = y;
+    d->formaDisparo = NULL;
+
+    return d;
+}
+
+int getIDDisparador(Disparador d) {
+    return ((disparador*)d)->id;
+}
+
+double getXDisparador(Disparador d) {
+    return ((disparador*)d)->x;
+}
+
+void setXDisparador(Disparador d,double x) {
+    ((disparador*)d)->x = x;
+}
+
+double getYDisparador(Disparador d) {
+    return ((disparador*)d)->y;
+}
+
+void setYDisparador(Disparador d,double y) {
+    ((disparador*)d)->y = y;
+}
+
+Forma getFormaDisparador(Disparador d) {
+    return ((disparador*)d)->formaDisparo;
+}
+
+void setFormaDisparador(Disparador d,Forma formaDisparo) {
+    ((disparador*)d)->formaDisparo = formaDisparo;
+}
+
+Carregador getCarregadorEsq(Disparador d) {
+    return ((disparador*)d)->carregadorEsq;
+}
+
+Carregador getCarregadorDir(Disparador d) {
+    return ((disparador*)d)->carregadorDir;
+}
+
+void setCarregadoresDisparador(Disparador d, Carregador esq, Carregador dir) {
+    ((disparador*)d)->carregadorEsq = esq;
+    ((disparador*)d)->carregadorDir = dir;
+}
+
+void dispararDisparador(Disparador d, double dx,double dy, Fila arena) {
+
+}
