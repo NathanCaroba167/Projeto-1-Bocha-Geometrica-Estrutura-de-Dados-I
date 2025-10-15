0b1d2b3cb66273a0908dbfbf8d22822e02af39f9
Nathan Caroba <nathancaroba01@gmail.com>
2025-10-15 15:01:17 -0300

    docs(svg): atualiza instruções

--- src/svg.h
+++ src/svg.h
@@ -2,7 +2,75 @@
 // Created by natha on 04/10/2025.
 //
 
-#ifndef CLION_SVG_H
-#define CLION_SVG_H
+#ifndef SVG_H
+#define SVG_H
 
-#endif //CLION_SVG_H
+/*
+ Módulo encarregado de ler um arquivo (.svg) e executar os comandos com as operações de abrir e ler
+ */
+
+/*
+ Ponteiro do tipo FILE Arquivo
+ Ponteiro char Nome
+ */
+typedef FILE* Arquivo;
+typedef char* Nome;
+
+
+/// @brief Cria e abre o arquivo SVG para escrita
+/// @param svg Ponteiro para o arquivo que será criado e aberto
+/// @param arquivo Ponteiro para o nome do arquivo que será criado e aberto
+void abrirSVG(Arquivo *svg, Nome arquivo);
+
+/// @brief Adiciona a tag do arquivo SVG
+/// @param svg Ponteiro para o arquivo que será inicializado
+/// @warning
+void inicializarSVG(Arquivo svg);
+
+/// @brief Desenha um círculo no arquivo SVG
+/// @param svg Ponteiro para o arquivo que receberá o desenho
+/// @param c Círculo que será desenhado
+/// @warning
+void desenharCirculoSVG(Arquivo svg, Circulo c);
+
+/// @brief Desenha um retângulo no arquivo SVG
+/// @param svg Ponteiro para o arquivo que receberá o desenho
+/// @param r Retângulo que será desenhado
+/// @warning
+void desenharRetanguloSVG(Arquivo svg, Retangulo r);
+
+/// @brief Desenha uma linha no arquivo SVG
+/// @param svg Ponteiro para o arquivo que receberá o desenho
+/// @param l Linha que será desenhada
+/// @warning
+void desenharLinhaSVG(Arquivo svg, Linha l);
+
+/// @brief Desenha um texto no arquivo SVG
+/// @param svg Ponteiro para o arquivo que receberá o desenho
+/// @param t Texto que será desenhado
+/// @param e Estilo do texto que será desenhado
+/// @warning
+void desenharTextoSVG(Arquivo svg, Texto t, Estilo e);
+
+/// @brief Desenha um asterisco vermelho no local da forma esmagada
+/// @param svg Ponteiro para o arquivo que receberá o desenho
+/// @param x Coordenada x do asterisco
+/// @param y Coordenada y do asterisco
+/// @warning
+void desenharAsteriscoSVG(Arquivo svg, double x, double y);
+
+/// @brief Desenha as dimensões do disparo a partir do disparador
+/// @param svg Ponteiro para o arquivo que receberá o desenho
+/// @param d Ponteiro para o disparador que será a referência
+/// @param dx Distância da coordenada x a partir do disparador
+/// @param dy Distância da coordenada y a partir do disparador
+/// @warning
+void desenharDimensoesDisparoSVG(Arquivo svg, Disparador d, double dx, double dy);
+
+/// @brief Fecha o arquivo SVG
+/// @param svg Ponteiro para o arquivo que será fechado
+/// @warning
+void fecharSVG(Arquivo svg);
+
+
+#endif //SVG_H
