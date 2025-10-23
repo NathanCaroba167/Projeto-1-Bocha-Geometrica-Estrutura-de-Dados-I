dc6f5bdc8437644ab114f63fe8b717e18aca364f
Nathan Caroba <nathancaroba01@gmail.com>
2025-10-23 09:31:39 -0300

    feat(forma): adiciona comandos de get,set e clonar

--- src/forma.c
+++ src/forma.c
@@ -1,20 +1,22 @@
 //
 // Created by natha on 15/10/2025.
 //
-#include "geo.h"
-#include "forma.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "circulo.h"
+#include "retangulo.h"
+#include "linha.h"
+#include "texto.h"
+#include "forma.h"
 
-typedef struct pacote{
+typedef struct {
     Forma form;
     TipoForma tipo;
-};
+}pacote;
 
 Pacote CriarPacote(Forma g,TipoForma tipo) {
-    Pacote p = (Pacote)malloc(sizeof(Pacote));
+    pacote* p = (Pacote)malloc(sizeof(pacote*));
     if (p == NULL) {
         printf("Erro ao alocar memoria!\n");
         exit(1);
@@ -26,13 +28,194 @@ Pacote CriarPacote(Forma g,TipoForma tipo) {
 }
 
 Forma getDadosForma(Pacote p) {
-    return p ? p->form: NULL;
+    return ((pacote*)p)->form;
 
 }
 
 TipoForma getTipoForma(Pacote p) {
-    return p ? p->tipo: -1;
+    return ((pacote*)p)->tipo;
 
+}
+
+double getAreaForma(Pacote p) {
+    TipoForma tipo = getTipoForma(p);
+    switch (tipo) {
+        case CIRCULO:
+            Circulo c = getDadosForma(p);
+            return calcAreaCirculo(c);
+        case RETANGULO:
+            Retangulo r = getDadosForma(p);
+            return calcAreaRetangulo(r);
+        case LINHA:
+            Linha l = getDadosForma(p);
+            return calcAreaLinha(l);
+        case TEXTO:
+            Texto t = getDadosForma(p);
+            return calcAreaTexto(t);
+        default:
+            printf("ERRO: tipo inválido!\n");
+            return 0.0;
+    }
+}
+
+int getIDForma(Pacote p) {
+    TipoForma tipo = getTipoForma(p);
+
+    switch (tipo) {
+        case CIRCULO:
+            Circulo c = getDadosForma(p);
+            return getIDCirculo(c);
+        case RETANGULO:
+            Retangulo r = getDadosForma(p);
+            return getIDRetangulo(r);
+        case LINHA:
+            Linha l = getDadosForma(p);
+            return getIDLinha(l);
+        case TEXTO:
+            Texto t = getDadosForma(p);
+            return getIDTexto(t);
+        default:
+            printf("ERRO: tipo inválido!\n");
+            return 0;
+    }
+}
+
+char* getCorBForma(Pacote p) {
+    TipoForma tipo = getTipoForma(p);
+
+    switch (tipo) {
+        case CIRCULO:
+            Circulo c = getDadosForma(p);
+            return getCorBCirculo(c);
+        case RETANGULO:
+            Retangulo r = getDadosForma(p);
+            return getCorBRetangulo(r);
+        case LINHA:
+            Linha l = getDadosForma(p);
+            return getCorLinha(l);
+        case TEXTO:
+            Texto t = getDadosForma(p);
+            return getCorBTexto(t);
+        default:
+            printf("ERRO: tipo inválido!\n");
+            return NULL;
+    }
+}
+
+char* getCorPForma(Pacote p) {
+    TipoForma tipo = getTipoForma(p);
+
+    switch (tipo) {
+        case CIRCULO:
+            Circulo c = getDadosForma(p);
+            return getCorPCirculo(c);
+        case RETANGULO:
+            Retangulo r = getDadosForma(p);
+            return getCorPRetangulo(r);
+        case LINHA:
+            Linha l = getDadosForma(p);
+            return getCorLinha(l);
+        case TEXTO:
+            Texto t = getDadosForma(p);
+            return getCorPTexto(t);
+        default:
+            printf("ERRO: tipo inválido!\n");
+            return NULL;
+    }
+}
+
+void setCorBForma(Pacote p, char* corB) {
+    TipoForma tipo = getTipoForma(p);
+
+    switch (tipo) {
+        case CIRCULO:
+            Circulo c = getDadosForma(p);
+            return setCorBCirculo(c,corB);
+        case RETANGULO:
+            Retangulo r = getDadosForma(p);
+            return setCorBRetangulo(r,corB);
+        case LINHA:
+            Linha l = getDadosForma(p);
+            return setCorLinha(l,corB);
+        case TEXTO:
+            Texto t = getDadosForma(p);
+            return setCorBTexto(t,corB);
+        default:
+            printf("ERRO: tipo inválido!\n");
+    }
+}
+
+void setCorPForma(Pacote p, char* corP) {
+    TipoForma tipo = getTipoForma(p);
+
+    switch (tipo) {
+        case CIRCULO:
+            Circulo c = getDadosForma(p);
+            return setCorBCirculo(c,corP);
+        case RETANGULO:
+            Retangulo r = getDadosForma(p);
+            return setCorBRetangulo(r,corP);
+        case LINHA:
+            Linha l = getDadosForma(p);
+            return setCorLinha(l,corP);
+        case TEXTO:
+            Texto t = getDadosForma(p);
+            return setCorBTexto(t,corP);
+        default:
+            printf("ERRO: tipo inválido!\n");
+    }
+}
+
+Pacote clonarForma(Pacote p) {
+    TipoForma tipo = getTipoForma(p);
+
+    switch (tipo) {
+        case CIRCULO: {
+            Circulo c = getDadosForma(p);
+            Circulo clone = CriarCirculo(getIDCirculo(c),
+                getXCirculo(c),
+                getYCirculo(c),
+                getRCirculo(c),
+                getCorBCirculo(c),
+                getCorPCirculo(c));
+            return CriarPacote(clone,CIRCULO);
+        }
+        case RETANGULO: {
+            Retangulo r = getDadosForma(p);
+            Retangulo clone = CriarRetangulo(getIDRetangulo(r),
+                getXRetangulo(r),
+                getYRetangulo(r),
+                getWRetangulo(r),
+                getHRetangulo(r),
+                getCorBForma(r),
+                getCorPRetangulo(r));
+            return CriarPacote(clone,RETANGULO);
+        }
+        case LINHA: {
+            Linha l = getDadosForma(p);
+            Linha clone = CriarLinha(getIDLinha(l),
+                getX1Linha(l),
+                getY1Linha(l),
+                getX2Linha(l),
+                getY2Linha(l),
+                getCorLinha(l));
+            return CriarPacote(clone,LINHA);
+        }
+        case TEXTO: {
+            Texto t = getDadosForma(p);
+            Texto clone = CriarTexto(getIDTexto(t),
+                getXTexto(t),
+                getYTexto(t),
+                getCorBTexto(t),
+                getCorPTexto(t),
+                getATexto(t),
+                getTxtoTexto(t));
+            return CriarPacote(clone,TEXTO);
+        }
+        default:
+            printf("ERRO: tipo inválido!\n");
+            return NULL;
+    }
 }
 
 void liberarForma(Pacote p) {
--- src/forma.h
+++ src/forma.h
@@ -5,6 +5,10 @@
 #ifndef FORMA_H
 #define FORMA_H
 
+/*
+ Módulo encarregado de executar e coordenar uma pilha com operações de iniciar, empilhar, desempilhar, get, 'pilhavazia' e liberar
+ */
+
 typedef enum {
     CIRCULO,
     RETANGULO,
@@ -12,8 +16,12 @@ typedef enum {
     TEXTO
 }TipoForma;
 
-typedef struct pacote* Pacote;
-
+/*
+ Ponteiro void Pacote
+ Ponteiro void Forma
+ */
+typedef void* Pacote;
+typedef void* Forma;
 
 /// @brief Cria o pacote da forma com o tipo dela
 /// @param g Ponteiro para forma que será empacotada
@@ -33,6 +41,46 @@ Forma getDadosForma(Pacote p);
 /// @return Ponteiro para o tipo de forma que foi pega
 /// @warning
 TipoForma getTipoForma(Pacote p);
+
+/// @brief Pega a area da forma
+/// @param p Ponteiro para a forma que a area será calculada
+/// @return O valor da área da forma calculada
+/// @warning
+double getAreaForma(Pacote p);
+
+/// @brief Pega o ID da forma sem modifica-la
+/// @param p Ponteiro para a forma que será pego o ID
+/// @return O ID da forma
+int getIDForma(Pacote p);
+
+/// @brief Pega a cor de borda da forma sem modifica-la
+/// @param p Ponteiro para a forma que será pega a cor de borda
+/// @return String com a cor de borda da forma
+/// @warning
+char* getCorBForma(Pacote p);
+
+/// @brief Pega a cor de preenchimento da forma sem modifica-la
+/// @param p Ponteiro para a forma que será pega a cor de preenchimento
+/// @return String com a cor de preenchimento da forma
+/// @warning
+char* getCorPForma(Pacote p);
+
+/// @brief Define a nova cor de borda da forma
+/// @param p Ponteiro para a forma onde a cor de borda será definida
+/// @param corB Nova cor de borda que será definida
+/// @warning
+void setCorBForma(Pacote p, char* corB);
+
+/// @brief Define a nova cor de preenchimento da forma
+/// @param p Ponteiro para a forma onde a cor de preenchimento será definida
+/// @param corP Nova cor de preenchimento que será definida
+/// @warning
+void setCorPForma(Pacote p, char* corP);
+
+/// @brief Clona uma forma
+/// @param p Ponteiro para forma que será clonada
+/// @return Ponteiro para forma clonada
+Pacote clonarForma(Pacote p);
 
 /// @brief Libera a memoria alocada para o pacote
 /// @param p Ponteiro para o pacote analisado
